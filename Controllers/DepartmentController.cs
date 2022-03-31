f99a24e4272db5a46ab71b43b4026a6228fd62e7
PallaviKatari <pallavikatari@gmail.com>
2022-03-31 19:14:30 +0530

    Add project files.

---
+++ Controllers/DepartmentController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MVC_CFA.Models;
+
+namespace MVC_CFA.Controllers
+{
+    public class DepartmentController : Controller
+    {
+        DepartmentEmployeecontext context=new DepartmentEmployeecontext();
+        // GET: Department
+        public ActionResult AddNewDepartment()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult AddNewDepartment(Department department)
+        {
+            //Binding of incoming values
+            if(ModelState.IsValid)
+            {
+                context.Departments.Add(department);
+                context.SaveChanges();
+            }
+            return View();
+        }
+    }
+}
---
+++ Controllers/EmployeeController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MVC_CFA.Models;
+
+namespace MVC_CFA.Controllers
+{
+    public class EmployeeController : Controller
+    {
+        DepartmentEmployeecontext context = new DepartmentEmployeecontext();
+        // GET: Department
+        public ActionResult AddNewEmployee()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult AddNewEmployee(Employee employee)
+        {
+            //Binding of incoming values
+            if (ModelState.IsValid)
+            {
+                context.Employees.Add(employee);
+                context.SaveChanges();
+            }
+            return View();
+        }
+    }
+}
